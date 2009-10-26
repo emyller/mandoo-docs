daae63db3423b40bb0dcacc6e1a906d21f3ee9b4
Evandro Myller <evandromyller@gmail.com>
2009-10-26 14:08:56 -0300

    some style-related confs

--- conf.py
+++ conf.py
@@ -96,14 +96,21 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'rightsidebar': True,
+    'stickysidebar': True,
+
+    'relbarbgcolor': '#000',
+    'sidebarbgcolor': '#333',
+    'sidebarlinkcolor': '#72a5e4',
+    'bodyfont': '"georgia", "serif"',}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Mandoo documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
